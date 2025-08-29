99ef28dff7db1c9a337f44136ce6ebc09fc83f11
muhammadxoja <muhammadxojaxiaomi@gmail.com>
2025-08-29 13:48:23 +0500

    kajfdnn

--- vite.config.ts
+++ vite.config.ts
@@ -27,9 +27,9 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"), // index.html shu papkada turishi kerak
+  root: "client", // index.html shu yerda
   build: {
-    outDir: path.resolve(__dirname, "dist"), // faqat dist, public emas!
+    outDir: "../dist", // dist loyihaning rootida bo‘ladi
     emptyOutDir: true,
   },
   server: {
